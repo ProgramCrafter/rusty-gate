eddba99fa294d6e6a21488ce21eee101b0803087
Andrey <pythonhunter146@gmail.com>
2023-02-08 19:30:55 +0500

    Application answering proxy requests

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,78 @@
-use std::error::Error;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::AsyncBufReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::io::BufReader;
+use tokio::io::Result;
 
-mod connections;    use connections::connect_to;
-mod ls_connect;     use ls_connect::{ServerKind, connect_to_any_ls};
-mod ac_extensions;  use ac_extensions::AdnlClientExt;
+use std::io::ErrorKind;
+use std::io::Error;
 
+#[tokio::main]
+async fn main() {
+  // 7651 = zlib.crc32(b'adnl-proxy') % 10000
+  let listener = TcpListener::bind("0.0.0.0:7651").await.unwrap();
 
-fn main() -> Result<(), Box<dyn Error>> {
-  let mut client = connect_to("chain-arrow-viewer.ton".to_owned())?;
+  loop {
+    let (socket, peer_addr) = listener.accept().await.unwrap();
+    tokio::spawn(async move {
+      println!("Started interaction with {}", peer_addr);
+      match serve_proxy(socket).await {
+        Ok(()) => println!("Finished interaction with {}", peer_addr),
+        Err(e) => println!("Got a error {e:?} while handling request from {}", peer_addr)
+      };
+    });
+  }
+}
+
+async fn respond_failure(mut wr: impl AsyncWriteExt+Unpin, reason: &str) -> Result<()> {
+  wr.write_all(b"HTTP/1.0 500 Internal Server Error\r\n").await?;
+  wr.write_all(b"Content-Type: text/plain\r\n").await?;
+  wr.write_all(b"Connection: close\r\n\r\n").await?;
+  wr.write_all(reason.as_bytes()).await?;
+  
+  Err(Error::new(ErrorKind::Other, reason))
+}
+
+async fn serve_proxy(mut socket: TcpStream) -> Result<()> {
+  let (rd, mut wr) = socket.split();
+  let mut rd = BufReader::new(rd);
+  
+  // Parsing the incoming request
+  
+  let mut proxy_to = String::new();
+  rd.read_line(&mut proxy_to).await?;
+  
+  let request_params: Vec<&str> = proxy_to.split(' ').collect();
+  let verb = request_params[0];
+  if verb == "PROXY" {
+    if request_params[1] != "TCP4" {
+      return respond_failure(wr, "unsupported protocol to forward").await;
+    }
+    
+    let dest_ip   = request_params[3];
+    let dest_port = request_params[5];
+    
+    let dest_pt = match dest_port.parse() {
+      Ok(port) => {port}
+      Err(_)   => {return respond_failure(wr, "invalid destination port").await;}
+    };
+    
+    let mut server_socket = TcpStream::connect((dest_ip, dest_pt)).await?;
+    
+    // copying buffer from `rd`
+    server_socket.write_all(rd.buffer()).await?;
+    drop(rd);
+    
+    tokio::io::copy_bidirectional(&mut socket, &mut server_socket).await?;
+  } else if verb == "GET" {
+    wr.write_all(b"HTTP/1.0 200 OK\r\n").await?;
+    wr.write_all(b"Content-Type: text/plain\r\n").await?;
+    wr.write_all(b"Connection: close\r\n\r\n").await?;
+    wr.write_all( format!("proxy_to: {proxy_to:?}\r\n").as_bytes() ).await?;
+    wr.write_all( format!("request:  {request_params:?}\r\n").as_bytes() ).await?;
+  } else {
+    respond_failure(wr, &format!("unexpected HTTP verb: {verb}")).await?;
+  }
   
   Ok(())
-  
-  /*
-  let mut client = connect_to_any_ls(ServerKind::LiteServerMainnet)?;
-
-  // already serialized TL with gettime query
-  let mut query = *b"z\xf9\x8b\xb45&>l\x95\xd6\xfe\xcbI}\xfd\n\xa5\xf01\xe7\xd4\x12\x98k\\\xe7 Im\xb5\x12\x05.\x8f-\x10\x0c\xdf\x06\x8cy\x044Z\xad\x16\x00\x00\x00\x00\x00\x00";
-
-  client.send_rand_nonce(&mut query)?;
-  
-  let mut result: Vec<u8> = Vec::new();
-  client.receive_se::<_, 8192>(&mut result)?;
-
-  // get time from serialized TL answer
-  println!(
-    "received: {}",
-    u32::from_le_bytes(result[result.len() - 7..result.len() - 3].try_into()?)
-  );
-  Ok(())
-  */
 }
